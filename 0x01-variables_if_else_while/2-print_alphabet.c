39e69085c03d8aacdc8157d03a776387a1f0a1cf
Allen Njiva <anjiva7@gmail.com>
2023-03-20 14:36:24 -0700

    Letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,17 +7,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+for (low = 'a'; low <= 'z'; low++)
 	{
 		putchar(low);
-	}	
-		putchar('\n');
-
-
-	return (0);
+}	
+putchar('\n');
+return (0);
 }
