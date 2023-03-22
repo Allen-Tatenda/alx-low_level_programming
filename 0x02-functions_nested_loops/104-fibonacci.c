7aabfb77f487df1fe2b0d077ed15cf1388d1ac2a
Allen Njiva <anjiva7@gmail.com>
2023-03-22 13:52:56 -0700

    Finished

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// if not the last number, print comma and space
+int main() {
+    int a = 1, b = 2, c, i;
+
+    printf("%d, %d, ", a, b);
+
+    for (i = 2; i < 98; i++) {
+        c = a + b;
+        printf("%d", c);
+
+        if (i != 97) {  // if not the last number, print comma and space
+            printf(", ");
+        }
+
+        a = b;
+        b = c;
+    }
+
+    printf("\n");
+
+    return 0;
+}
